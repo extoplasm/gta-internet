4fb168bfb31d0109698656665581ea732aaf7363
extoplasm <pleassehelsp@gmail.com>
2024-05-19 17:42:22 +1000

    working on history

--- gta-internet-remake/src/App.tsx
+++ gta-internet-remake/src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/header';
 
+let historyPageID = 0
+
+interface History {
+  id: number;
+  name: string;
+}
+
 export default function App() {
+  const [pageHistory, setPageHistory] = useState(Array<History>)
   return (
-    <Header 
-    preview="--- EYEFIND... it's like a series of tubes ---"
-    currentpage='www.eyefind.info'
-    />
+    <>
+      <Header 
+        preview="--- EYEFIND... it's like a series of tubes ---"
+        currentpage="www.eyefind.info"
+        history={pageHistory}
+      />
+      <button onClick={() => {
+        setPageHistory(
+          [
+            ...pageHistory, 
+            { id: historyPageID++, name: 'www.eyefind.info' }
+          ]
+        )
+      }}>go to eyefind.info</button>
+    </>
   );
 }
 
--- gta-internet-remake/src/components/header.tsx
+++ gta-internet-remake/src/components/header.tsx
@@ -1,11 +1,20 @@
 import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+interface History {
+    id: number;
+    name: string;
+}
 
 interface Props {
     preview: string;
     currentpage: string;
+    history: Array<History>;
 }
 
-export default function header({ preview, currentpage }: Props) {
+export default function header({ preview, currentpage, history }: Props) {
     return (
         <>
             <div id="title">
@@ -17,7 +26,19 @@ export default function header({ preview, currentpage }: Props) {
                     <div id="forward-arrow-button"></div>
                 </div>
                 <div id="home-button"></div>
-                <div id="history-button"></div>
+                <div id="history-button">
+                    <div id="button-icon">
+                        <FontAwesomeIcon icon={faClock} />
+                        <FontAwesomeIcon icon={faCaretDown} />
+                    </div>
+                    <div id="dropdown">
+                        <ul>
+                            {history.map(p => (
+                                <p key={p.id}>{p.name}</p>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
                 <div id="search-bar">
                     <div id="text-field">
                         {currentpage}
