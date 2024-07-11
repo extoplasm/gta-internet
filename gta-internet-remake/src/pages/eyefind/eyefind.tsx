816744780370e73dfc14121b1518f539e4a574d5
extoplasm <pleassehelsp@gmail.com>
2024-07-11 14:00:49 +1000

    reordered setup

--- gta-internet-remake/src/pages/eyefind/eyefind.tsx
+++ gta-internet-remake/src/pages/eyefind/eyefind.tsx
@@ -1,7 +1,5 @@
 import './eyefind.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faSun, faMobileButton } from '@fortawesome/free-solid-svg-icons'
-import pageData from '../../assets/pagedata.json'
+import Navbar from './components/navbar'
 
 interface Props {
     currentPage: string;
@@ -23,19 +21,6 @@ const getQueryString = (page: string) => {
     return match ? match[1] : '';
 }
 
-const getRandomPage = (data : object) => {
-    const pages = Object.keys(data);
-    const randomIndex = Math.floor(Math.random() * pages.length)
-    return pages[randomIndex]
-}
-
-const getCurrentDay = () => {
-    const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
-    const curr = new Date;
-
-    return days[curr.getDay()]
-}
-
 export default function Page({ currentPage, addNewPage }: Props) {
     // @ts-expect-error (not sure why this keeps happening)
     const PageComponent = subPages[getQueryString(currentPage)]
@@ -47,63 +32,6 @@ export default function Page({ currentPage, addNewPage }: Props) {
             />
         </>
     );
-}
-
-function Navbar({ addNewPage }: pageProps) {
-    return (
-        <>
-            <div id="eyefind-navbar">
-                <div className="banner">
-                    <div className="info">
-                        <img className="logo" src="../../eyefind-logo.png" alt="logo" />
-                        <div className="weather">
-                            <div className="weather-text">
-                                <p>Rockford Hills</p>
-                                <p>{getCurrentDay()}</p>
-                            </div>
-                            <div className="weather-icon">
-                                <FontAwesomeIcon icon={faSun}/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="page-nav">
-                        <div className="search-bar">
-                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                            <input 
-                                className="search-field" 
-                                type="text"
-                                autoComplete="off"
-                                placeholder="Search Eyefind" 
-                            />
-                        </div>
-                        <button className="random" onClick={() => 
-                            addNewPage(getRandomPage(pageData))
-                        }>
-                            RANDOM
-                        </button>
-                    </div>
-                </div>
-                <div className="page-categories">
-                    <div className="media">
-                        <FontAwesomeIcon icon={faMobileButton}/>
-                        <p>MEDIA AND ENTERTAINMENT</p>
-                    </div>
-                    <div className="food">
-                        <p>FOOD AND DRINK</p>
-                    </div>
-                    <div className="money">
-                        <p>MONEY AND SERVICES</p>
-                    </div>
-                    <div className="travel">
-                        <p>TRAVEL AND TRANSPORT</p>
-                    </div>
-                    <div className="fashion">
-                        <p>FASHION AND HEALTH</p>
-                    </div>
-                </div>
-            </div>  
-        </>
-    )
 }
 
 function ErrorPage({ addNewPage }: pageProps) {
